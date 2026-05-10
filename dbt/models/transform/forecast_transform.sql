c8b90e284790317a8305e755fa8539dfeacb7f6d
Priyank Mehta <54474715+priyankmehta5@users.noreply.github.com>
2026-05-10 12:36:50 -0700

    Updated final dbt models

---
+++ dbt/models/transform/forecast_transform.sql
@@ -0,0 +1,47 @@
+WITH fire_forecast AS (
+    SELECT *
+    FROM {{ source('analytics', 'nifc_fire_forecast_updated') }}
+),
+
+aqi_forecast AS (
+    SELECT *
+    FROM {{ source('analytics', 'aqi_forecast_with_parameter') }}
+),
+
+joined AS (
+    SELECT
+        f.*,
+
+        a.aqi,
+        a.parameter AS aqi_parameter,
+        a.is_forecast AS aqi_is_forecast,
+        a.forecast_generated_at AS aqi_forecast_generated_at,
+
+        CASE
+            WHEN MONTH(f.date) IN (12, 1, 2) THEN 'Winter'
+            WHEN MONTH(f.date) IN (3, 4, 5) THEN 'Spring'
+            WHEN MONTH(f.date) IN (6, 7, 8) THEN 'Summer'
+            ELSE 'Fall'
+        END AS season,
+
+        LEAST(
+            100,
+            COALESCE(f.incident_count, 0) * 2
+            + CASE WHEN COALESCE(f.total_acres, 0) > 50 THEN 20 ELSE 0 END
+            + CASE WHEN COALESCE(f.max_acres, 0) > 20 THEN 10 ELSE 0 END
+        ) AS wildfire_risk_score,
+
+        CASE
+            WHEN a.aqi <= 50 THEN 'Good'
+            WHEN a.aqi <= 100 THEN 'Bad'
+            ELSE 'Terrible'
+        END AS aqi_range
+
+    FROM fire_forecast f
+    LEFT JOIN aqi_forecast a
+        ON f.city = a.city
+       AND f.date = a.date
+)
+
+SELECT *
+FROM joined
